65443b2558d3b3e04b120821e772217b6aea1790
user <gngh0101@gmail.com>
2020-01-14 23:37:12 +0900

    Add cache of set for test

--- src/main/java/com/github/hotire/spring/data/redis/message/Receiver.java
+++ src/main/java/com/github/hotire/spring/data/redis/message/Receiver.java
@@ -1,6 +1,9 @@
 package com.github.hotire.spring.data.redis.message;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,10 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class Receiver {
 
+  @Getter
+  private final Set<String> cache = new HashSet<>();
   private final CountDownLatch latch;
 
   public void receiveMessage(final String message) {
     log.info("Received < {} > ", message);
+    cache.add(message);
     latch.countDown();
   }
 }
