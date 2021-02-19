ca87ff5ed843ea1e76af37313496f4266f5fb691
hotire <gngh0101@gmail.com>
2021-02-20 00:42:08 +0900

    Learn how to failover redis

--- README.md
+++ README.md
@@ -36,3 +36,12 @@ redis shutdown
 
 https://github.com/ozimov/embedded-redis
 
+### Redis Failover
+- enablePeriodicRefresh(default = false) : 주기적으로 cluster topology를 refresh 여부를 결정 
+- enableAdaptiveRefreshTrigger(default = none) : cluster topology를 refresh를 trigger할 event를 설정할 수 있음 
+  - MOVED_REDIRECT : required key가 다른 node에 있는 경우 
+  - ASK_REDIRECT : slot migration이 진행되는 경우, 해당 node에 데이터가 있지만 곧 타 node로 이동될 경우 
+  - PERSISTENT_RECONNECTS : connection 문제
+  - UNKNOWN_NODE : 알수 없는 node
+  - UNCOVERED_SLOT : 
+
--- src/main/java/com/github/hotire/spring/data/redis/RedisConfig.java
+++ src/main/java/com/github/hotire/spring/data/redis/RedisConfig.java
@@ -1,15 +1,23 @@
 package com.github.hotire.spring.data.redis;
 
+import java.time.Duration;
+
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 
 @Configuration
 public class RedisConfig {
     @Bean
     public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
-        return cb -> cb.clientOptions(ClusterClientOptions.builder().build());
+        return cb -> cb.clientOptions(ClusterClientOptions.builder()
+                                                          .topologyRefreshOptions(ClusterTopologyRefreshOptions.builder()
+                                                                                                               .refreshPeriod(Duration.ofMinutes(15))
+                                                                                                               .enableAdaptiveRefreshTrigger()
+                                                                                                               .build())
+                                                          .build());
     }
 }
