47afaf7586b0d8c4f09a58211a139210829aa486
hotire <gngh0101@gmail.com>
2022-11-09 00:39:20 +0900

    Add SimpleKeyValueRepositoryCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/repository/SimpleKeyValueRepositoryCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.repository;
+
+import  org.springframework.data.keyvalue.repository.support.SimpleKeyValueRepository;
+
+/**
+ * @see SimpleKeyValueRepository
+ */
+public class SimpleKeyValueRepositoryCore {
+
+}
