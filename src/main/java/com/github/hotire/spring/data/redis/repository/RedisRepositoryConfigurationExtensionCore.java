389806d64c17850ed1cba33fa48d183809847a19
hotire <gngh0101@gmail.com>
2022-11-09 00:39:40 +0900

    Add RedisRepositoryConfigurationExtensionCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/repository/RedisRepositoryConfigurationExtensionCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.spring.data.redis.repository;
+
+import org.springframework.data.redis.repository.configuration.RedisRepositoryConfigurationExtension;
+
+
+/**
+ * @see RedisRepositoryConfigurationExtension
+ */
+public class RedisRepositoryConfigurationExtensionCore {
+
+}
