79fc462b2fdb8c84a37d598e932c743cd54796e2
hotire <gngh0101@gmail.com>
2022-11-09 00:40:59 +0900

    Add RedisTemplateCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/RedisTemplateCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.core;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @see RedisTemplate
+ */
+public class RedisTemplateCore {
+
+}
