32eb8a2e16e0fed4281bf4bbb2c8008a8bff26ea
hotire <gngh0101@gmail.com>
2022-11-09 00:44:23 +0900

    Add RedisConnectionFactoryCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/connection/RedisConnectionFactoryCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.core.connection;
+
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+/**
+ * @see RedisConnectionFactory
+ */
+public class RedisConnectionFactoryCore {
+
+}
