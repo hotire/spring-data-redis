34b5de63112b1178257fd5d1ab8385e36fbd87d4
hotire <gngh0101@gmail.com>
2022-11-09 00:33:39 +0900

    Learn EnableRedisRepositoriesCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/repository/EnableRedisRepositoriesCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.repository;
+
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+/**
+ * @see EnableRedisRepositories
+ */
+public class EnableRedisRepositoriesCore {
+
+}
