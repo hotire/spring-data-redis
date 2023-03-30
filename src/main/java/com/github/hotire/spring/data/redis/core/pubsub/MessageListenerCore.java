6741b7d9fe73f3a99532b7cbb0ecb7905ebea58d
hotire <gngh0101@gmail.com>
2023-03-31 03:07:09 +0900

    Add MessageListenerCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/pubsub/MessageListenerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.core.pubsub;
+
+import org.springframework.data.redis.connection.MessageListener;
+
+/**
+ * @see MessageListener
+ */
+public class MessageListenerCore {
+
+}
