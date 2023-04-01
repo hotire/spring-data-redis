0bb61c6837967087315f6fc3546f524f0262adad
hotire <gngh0101@gmail.com>
2023-04-01 12:06:02 +0900

    Add RedisMessageListenerContainerCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/pubsub/RedisMessageListenerContainerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.core.pubsub;
+
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @see RedisMessageListenerContainer
+ */
+public class RedisMessageListenerContainerCore {
+
+}
