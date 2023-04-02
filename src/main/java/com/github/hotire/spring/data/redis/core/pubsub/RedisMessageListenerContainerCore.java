9224a837000be4f28db367fde5cea93e405434ed
hotire <gngh0101@gmail.com>
2023-04-03 00:31:09 +0900

    Add addMessageListener

--- src/main/java/com/github/hotire/spring/data/redis/core/pubsub/RedisMessageListenerContainerCore.java
+++ src/main/java/com/github/hotire/spring/data/redis/core/pubsub/RedisMessageListenerContainerCore.java
@@ -1,9 +1,12 @@
 package com.github.hotire.spring.data.redis.core.pubsub;
 
+import java.util.Collection;
 import java.util.concurrent.Executor;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.Topic;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ClassUtils;
 
@@ -34,5 +37,12 @@ public class RedisMessageListenerContainerCore {
     protected TaskExecutor createDefaultTaskExecutor() {
         String threadNamePrefix = (beanName != null ? beanName + "-" : DEFAULT_THREAD_NAME_PREFIX);
         return new SimpleAsyncTaskExecutor(threadNamePrefix);
+    }
+
+    /**
+     * @see RedisMessageListenerContainer#addMessageListener(MessageListener, Topic) 
+     */
+    public void addMessageListener(MessageListener listener, Collection<? extends Topic> topics) {
+        
     }
 }
