57c9484117fd6d0decf56518af8821215d1b40ee
hotire <gngh0101@gmail.com>
2023-04-03 00:39:01 +0900

    User messageListenerBean instaed of MessageListenerAdapter

--- src/main/java/com/github/hotire/spring/data/redis/pubsub/PubSubConfig.java
+++ src/main/java/com/github/hotire/spring/data/redis/pubsub/PubSubConfig.java
@@ -1,14 +1,14 @@
 package com.github.hotire.spring.data.redis.pubsub;
 
+import com.github.hotire.spring.data.redis.RedisTemplateFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-
-import com.github.hotire.spring.data.redis.RedisTemplateFactory;
 
 @Configuration
 public class PubSubConfig {
@@ -29,11 +29,16 @@ public class PubSubConfig {
     }
 
     @Bean
+    public MessageListener messageListenerBean() {
+        return new RedisMessageSubscriber();
+    }
+
+    @Bean
     public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container
                 = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(messageListener(), topic());
+        container.addMessageListener(messageListenerBean(), topic());
         return container;
     }
 
