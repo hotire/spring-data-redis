65dd0c1a74d59d5de2d272d4dff065c0ecb574a0
hotire <gngh0101@gmail.com>
2023-04-03 00:45:24 +0900

    Add ConvertAndSendCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/pubsub/ConvertAndSendCore.java
@@ -0,0 +1,16 @@
+package com.github.hotire.spring.data.redis.core.pubsub;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @see RedisTemplate
+ */
+public class ConvertAndSendCore {
+
+    /**
+     * @see RedisTemplate#convertAndSend(String, Object) 
+     */
+    public void convertAndSend(String channel, Object message) {
+
+    }
+}
