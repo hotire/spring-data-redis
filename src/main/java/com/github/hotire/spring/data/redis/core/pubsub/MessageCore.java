326e1ef670bc272c9a7b6dbb00833bd1e4a023da
hotire <gngh0101@gmail.com>
2023-04-08 19:49:48 +0900

    Add MessageCore

--- src/main/java/com/github/hotire/spring/data/redis/core/pubsub/MessageCore.java
+++ src/main/java/com/github/hotire/spring/data/redis/core/pubsub/MessageCore.java
@@ -1,9 +1,12 @@
 package com.github.hotire.spring.data.redis.core.pubsub;
 
 import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.DefaultMessage;
+
 
 /**
  * @see Message
+ * @see DefaultMessage
  */
 public class MessageCore {
 
