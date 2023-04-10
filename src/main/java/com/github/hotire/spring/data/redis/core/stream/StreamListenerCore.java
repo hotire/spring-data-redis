dd807e919d9c8284df0e65bdfb57c7a92abd22c0
hotire <gngh0101@gmail.com>
2023-04-11 02:47:45 +0900

    Add StreamListenerCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/stream/StreamListenerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.core.stream;
+
+import org.springframework.data.redis.stream.StreamListener
+
+/**
+ * @see StreamListener
+ */
+public class StreamListenerCore {
+
+}
