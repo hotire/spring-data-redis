c13feed2e336ddede956d7dd08b1fbfa3be4b31f
hotire <gngh0101@gmail.com>
2023-05-17 02:44:51 +0900

    add, members in SetOperations

--- src/main/java/com/github/hotire/spring/data/redis/core/SetOperationsCore.java
+++ src/main/java/com/github/hotire/spring/data/redis/core/SetOperationsCore.java
@@ -1,10 +1,25 @@
 package com.github.hotire.spring.data.redis.core;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.data.redis.core.SetOperations;
 
 /**
  * @see SetOperations
  */
-public class SetOperationsCore {
+public class SetOperationsCore<K, V> {
 
+    /**
+     * @see SetOperations#add(Object, Object[])
+     */
+    Long add(K key, V... values) {
+        return 0L;
+    }
+
+    /**
+     * @see SetOperations#members(Object)
+     */
+    Set<V> members(K key) {
+        return new HashSet<>();
+    }
 }
