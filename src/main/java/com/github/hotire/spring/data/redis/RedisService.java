af159071ccc6f162be09ffcff8fc8df6cca290d8
hotire <gngh0101@gmail.com>
2023-06-15 03:17:01 +0900

    Implement RedisService

--- src/main/java/com/github/hotire/spring/data/redis/RedisService.java
+++ src/main/java/com/github/hotire/spring/data/redis/RedisService.java
@@ -1,5 +1,8 @@
 package com.github.hotire.spring.data.redis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -10,7 +13,7 @@ import org.springframework.stereotype.Service;
 public interface RedisService {
 
 
-    <T> void save(String key, T value);
+    <T> void save(final String key, final T value, final Duration timeout, final Class<T> type);
 
 
     @Service
@@ -18,10 +21,22 @@ public interface RedisService {
     class Default implements RedisService {
 
         private final RedisTemplate<String, String> redisTemplate;
+        private final ObjectMapper objectMapper;
 
         @Override
-        public <T> void save(String key, T value) {
+        public <T> void save(final String key, final T value, final Duration timeout, final Class<T> type) {
+            if (String.class.equals(type)) {
+                final String casted = (String) value;
+                redisTemplate.opsForValue().set(key, casted, timeout);
+                return;
+            }
 
+            try {
+                final String json = objectMapper.writeValueAsString(value);
+                redisTemplate.opsForValue().set(key, json, timeout);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
         }
     }
 }
