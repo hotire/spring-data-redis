4a96365fcfc9142977899f6d0ec39b14c4fb812a
hotire <gngh0101@gmail.com>
2023-06-26 00:10:31 +0900

    Implement addToSet

--- src/main/java/com/github/hotire/spring/data/redis/RedisService.java
+++ src/main/java/com/github/hotire/spring/data/redis/RedisService.java
@@ -3,6 +3,7 @@ package com.github.hotire.spring.data.redis;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ public interface RedisService {
 
 
     <T> void save(final String key, final T value, final Duration timeout, final Class<T> type);
+    <T> void addToSet(final String key, final T value, final Duration timeout, final Class<T> type);
 
 
     @Service
@@ -34,6 +36,25 @@ public interface RedisService {
             try {
                 final String json = objectMapper.writeValueAsString(value);
                 redisTemplate.opsForValue().set(key, json, timeout);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+
+        @Override
+        public <T> void addToSet(String key, T value, Duration timeout,
+            Class<T> type) {
+            if (String.class.equals(type)) {
+                final String casted = (String) value;
+                redisTemplate.opsForSet().add(key, casted);
+                redisTemplate.expire(key, timeout.toMillis(), TimeUnit.MILLISECONDS);
+                return;
+            }
+
+            try {
+                final String json = objectMapper.writeValueAsString(value);
+                redisTemplate.opsForSet().add(key, json);
+                redisTemplate.expire(key, timeout.toMillis(), TimeUnit.MILLISECONDS);
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e.getMessage(), e);
             }
