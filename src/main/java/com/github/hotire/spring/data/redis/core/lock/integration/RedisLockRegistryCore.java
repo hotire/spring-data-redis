5c377ae6171b6677e0daaffa00d7d4664b68cbf9
hotire <gngh0101@gmail.com>
2023-08-07 00:08:53 +0900

    Add RedisLockRegistryCore

---
+++ src/main/java/com/github/hotire/spring/data/redis/core/lock/integration/RedisLockRegistryCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.data.redis.core.lock.integration;
+
+import org.springframework.integration.redis.util.RedisLockRegistry;
+
+/**
+ * @see RedisLockRegistry
+ */
+public class RedisLockRegistryCore {
+
+}
