01b5765404b7048fd88269da76cf12b72eac6dc6
hotire <gngh0101@gmail.com>
2023-08-08 00:09:16 +0900

    Add KeyValueRepositoryFactory

--- src/main/java/com/github/hotire/spring/data/redis/core/repository/KeyValueRepositoryFactoryCore.java
+++ src/main/java/com/github/hotire/spring/data/redis/core/repository/KeyValueRepositoryFactoryCore.java
@@ -1,5 +1,10 @@
 package com.github.hotire.spring.data.redis.core.repository;
 
+import org.springframework.data.keyvalue.repository.support.KeyValueRepositoryFactory;
+
+/**
+ * @see KeyValueRepositoryFactory
+ */
 public class KeyValueRepositoryFactoryCore {
 
 }
