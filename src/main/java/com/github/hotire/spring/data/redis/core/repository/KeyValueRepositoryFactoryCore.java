52c9cecf3d1b5da5f7638430a179d5b4c8e4c85a
hotire <gngh0101@gmail.com>
2023-08-08 00:12:24 +0900

    update KeyValueRepositoryFactoryCore

--- src/main/java/com/github/hotire/spring/data/redis/core/repository/KeyValueRepositoryFactoryCore.java
+++ src/main/java/com/github/hotire/spring/data/redis/core/repository/KeyValueRepositoryFactoryCore.java
@@ -1,9 +1,13 @@
 package com.github.hotire.spring.data.redis.core.repository;
 
 import org.springframework.data.keyvalue.repository.support.KeyValueRepositoryFactory;
+import org.springframework.data.redis.repository.support.RedisRepositoryFactory;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 
 /**
  * @see KeyValueRepositoryFactory
+ * @see RedisRepositoryFactory
+ * @see RepositoryFactorySupport
  */
 public class KeyValueRepositoryFactoryCore {
 
