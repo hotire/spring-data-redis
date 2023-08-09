264bc162952f20533b786202fcdd044c338da9a7
hotire <gngh0101@gmail.com>
2023-08-09 20:27:58 +0900

    Add RedisRepositoriesRegistrarCore

--- src/main/java/com/github/hotire/spring/data/redis/core/repository/RedisRepositoriesRegistrarCore.java
+++ src/main/java/com/github/hotire/spring/data/redis/core/repository/RedisRepositoriesRegistrarCore.java
@@ -1,12 +1,10 @@
 package com.github.hotire.spring.data.redis.core.repository;
 
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.repository.configuration.RedisRepositoriesRegistrar;
 
 /**
- * @see EnableRedisRepositories
  * @see RedisRepositoriesRegistrar
  */
-public class EnableRedisRepositoriesCore {
+public class RedisRepositoriesRegistrarCore {
 
 }
